f780438e4dc8f0d30163faa2ad0a3ceefa05666c
井ノ口 孝輝 <inokutitakaki@yahoo.co.jp>
2025-12-11 20:50:36 +0900

    test

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,7 @@
 
 const Header = () => {
   return (
-    <div className="fixed top-0 w-full h-16 flex justify-center text-4xl font-bold bg-green-600 text-white py-4">
+    <div className="w-full h-16 flex justify-center text-4xl font-bold bg-green-600 text-white py-4">
       コミュトレ!
     </div>
   );
